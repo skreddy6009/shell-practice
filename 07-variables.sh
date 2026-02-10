70678759a9081075b0b936243f89f5ff09701e7b
sivakumarreddy <skreddy6009@gmail.com>
2026-02-10 08:05:04 +0530

    meaningful message

--- 07-variables.sh
+++ 07-variables.sh
@@ -7,7 +7,8 @@ START_TIME=$(date +%s)
 sleep 10 &
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$($END_TIME-START_TIME)
+
+TOTAL_TIME=$(($END_TIME-START_TIME))
 
 echo "Script executed in: $TOTAL_TIME Seconds"
 
